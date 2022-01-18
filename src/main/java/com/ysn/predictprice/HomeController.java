c1cb8f08421fb5f456a8d019154d2913f0ef5c96
Seungnock Yeom <seungnock.yeom@encora.com>
2022-01-18 00:56:01 -0500

    thymeleaf basic

---
+++ src/main/java/com/ysn/predictprice/HomeController.java
@@ -0,0 +1,28 @@
+package com.ysn.predictprice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView goHome(HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView();
+        List<String> resultList = new ArrayList<String>();
+        resultList.add("AAA");
+        resultList.add("BBB");
+        resultList.add("CCC");
+        resultList.add("DDD");
+        resultList.add("EEE");
+        resultList.add("FFF");
+        mav.addObject("resultList", resultList);
+        mav.setViewName("contents/home");
+        return mav;
+    }
+}
