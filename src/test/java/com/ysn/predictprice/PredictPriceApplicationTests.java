daa9b21a90da2b42a3ce0e8d228ac4ec5a7cde76
Seungnock Yeom <seungnock.yeom@encora.com>
2022-01-17 23:32:40 -0500

    initial

---
+++ src/test/java/com/ysn/predictprice/PredictPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysn.predictprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
