70046bc86851ae80ff761bf26caeeeddab0e6a1b
Seungnock Yeom <frogengine@gmail.com>
2022-10-28 19:04:39 -0400

    select ticker asc

--- src/main/java/com/ysn/predictprice/entities/StockPredictionRepository.java
+++ src/main/java/com/ysn/predictprice/entities/StockPredictionRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 @Repository
 public interface StockPredictionRepository extends JpaRepository<StockPrediction, Long> {
-    @Query(value = "SELECT predict_date,s.ticker as ticker,accuracy,company_name,consensus,predict_time,prediction_today,price_yesterday,predicted_change_rate,yesterday_date,status FROM stock_prediction s INNER JOIN( SELECT ticker AS tick, max(predict_date) AS lastdate FROM stock_prediction GROUP BY tick ) sm ON s.ticker = sm.tick AND s.predict_date=sm.lastdate", nativeQuery = true)
+    @Query(value = "SELECT predict_date,s.ticker as ticker,accuracy,company_name,consensus,predict_time,prediction_today,price_yesterday,predicted_change_rate,yesterday_date,status FROM stock_prediction s INNER JOIN( SELECT ticker AS tick, max(predict_date) AS lastdate FROM stock_prediction GROUP BY tick ) sm ON s.ticker = sm.tick AND s.predict_date=sm.lastdate order by s.ticker asc", nativeQuery = true)
     List<StockPrediction> listFromLastOnes();
 }
