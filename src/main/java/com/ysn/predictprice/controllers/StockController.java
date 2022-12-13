d7d8d75f390b4b26ec46b62762c85b197d0a5c78
seyeom <seyeom@kissusa.com>
2022-12-13 16:44:27 -0500

    change index from date to number

--- src/main/java/com/ysn/predictprice/controllers/StockController.java
+++ src/main/java/com/ysn/predictprice/controllers/StockController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.Format;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Controller
@@ -32,6 +34,11 @@ public class StockController {
     public String predictiondetail(HttpServletRequest request, Model model) throws ParseException {
         String ticker = request.getParameter("ticker");
         model.addAttribute("ticker", ticker);
+        String bd = request.getParameter("bd");
+        if(bd == null) {
+            bd = "15";
+        }
+        model.addAttribute("bd",bd);
         model.addAttribute("queryPrediction", stockService.queryPrediction(ticker));
         model.addAttribute("queryReal", stockService.queryReal(ticker));
         return "contents/predictiondetail.html";
